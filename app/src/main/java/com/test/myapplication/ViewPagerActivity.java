4547ded596af63cf518ab236c7ddd96232d1615a
Renáta <renatandreea@gmail.com>
2019-03-22 20:49:10 +0200

    Not appearing PageIndicatorView

--- app/src/main/java/com/test/myapplication/ViewPagerActivity.java
+++ app/src/main/java/com/test/myapplication/ViewPagerActivity.java
@@ -2,6 +2,8 @@ package com.test.myapplication;
 
 import android.os.Bundle;
 
+import com.rd.PageIndicatorView;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
@@ -14,7 +16,24 @@ public class ViewPagerActivity extends AppCompatActivity {
 
         ViewPager viewPager = findViewById(R.id.viewpager);
         viewPager.setAdapter(new CustomPagerAdapter(this));
-    }
 
+        final PageIndicatorView pageIndicatorView = findViewById(R.id.pageIndicatorView);
+        pageIndicatorView.setCount(4); // specify total count of indicators
+        pageIndicatorView.setSelection(2);
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {/*empty*/}
+
+            @Override
+            public void onPageSelected(int position) {
+                pageIndicatorView.setSelection(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {/*empty*/}
+        });
+    }
 }
+
 
