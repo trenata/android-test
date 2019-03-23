62e3651ee915a1a6b15b5a51751363db96080a7f
Renáta <renatandreea@gmail.com>
2019-03-23 13:09:58 +0200

    Keep only one page

--- app/src/main/java/com/test/myapplication/CustomPagerAdapter.java
+++ app/src/main/java/com/test/myapplication/CustomPagerAdapter.java
@@ -4,22 +4,38 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.viewpager.widget.PagerAdapter;
 
 public class CustomPagerAdapter extends PagerAdapter {
 
-    private Context mContext;
+    private String[] titles;
+    private String[] descriptions;
+    private int[] images = {
+            R.drawable.onboarding_image_1,
+            R.drawable.onboarding_image_2,
+            R.drawable.onboarding_image_3,
+            R.drawable.onboarding_image_4
+    };
+    private LayoutInflater inflater;
 
-    public CustomPagerAdapter(Context context) {
-        mContext = context;
+    CustomPagerAdapter(Context context) {
+        inflater = LayoutInflater.from(context);
+        titles = context.getResources().getStringArray(R.array.titles);
+        descriptions = context.getResources().getStringArray(R.array.descriptions);
     }
 
     @Override
     public Object instantiateItem(ViewGroup collection, int position) {
-        CustomPagerEnum customPagerEnum = CustomPagerEnum.values()[position];
-        LayoutInflater inflater = LayoutInflater.from(mContext);
-        ViewGroup layout = (ViewGroup) inflater.inflate(customPagerEnum.getLayoutResId(), collection, false);
+        ViewGroup layout = (ViewGroup) inflater.inflate(R.layout.view_page, collection, false);
+        TextView title = layout.findViewById(R.id.title);
+        TextView description = layout.findViewById(R.id.description);
+        ImageView image = layout.findViewById(R.id.background_image);
+        title.setText(titles[position]);
+        description.setText(descriptions[position]);
+        image.setImageResource(images[position]);
         collection.addView(layout);
         return layout;
     }
@@ -31,18 +47,12 @@ public class CustomPagerAdapter extends PagerAdapter {
 
     @Override
     public int getCount() {
-        return CustomPagerEnum.values().length;
+        return titles.length;
     }
 
     @Override
     public boolean isViewFromObject(View view, Object object) {
         return view == object;
-    }
-
-    @Override
-    public CharSequence getPageTitle(int position) {
-        CustomPagerEnum customPagerEnum = CustomPagerEnum.values()[position];
-        return mContext.getString(customPagerEnum.getTitleResId());
     }
 
 }
